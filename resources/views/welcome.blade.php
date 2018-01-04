bc89f428254d9713ad55c2184c2b666eafec2aa6
Jonathan Hilfiker <jphilfiker@gmail.com>
2018-01-04 16:41:48 -0500

    testing commit
    changed laracasts to laravel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laracasts
+                    Laravel
                 </div>
 
                 <div class="links">

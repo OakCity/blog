7bc808f6751b2a4164ce4d052dd9896eab5507cb
Jonathan Hilfiker <jphilfiker@gmail.com>
2018-01-09 23:30:39 -0500

    Modified Index Page
    add posts to show all posts and added styling and linkage.

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -4,7 +4,7 @@
 
 <!-- Bootstrap CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
-
+{{ Html::style('css/styles.css') }}
 
 <title>Laravel Blog @yield('title') </title>
 
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,50 @@
+@extends('main')
+
+@section('title', '| All Posts')
+
+@section('content')
+
+    <div class="row">
+        <div class="col-md-10">
+            <h1>All Posts</h1>
+        </div>
+
+        <div class="col-md-2">
+             <a href="{{ route('posts.create') }}" class="btn btn-lg btn-block btn-primary">Create new Post</a>
+        </div>
+        <div class="col-md-12">
+        <hr>
+        </div>
+    </div>
+
+    <div class="row">
+        <table class="table">
+            <thead>
+                <th>#</th>
+                <th>Title</th>
+                <th>Body</th>
+                <th>Created At</th>
+                <th></th>
+            </thead>
+            <tbody>
+                @foreach ($posts as $post)
+                    <tr>
+                        <th>{{ $post->id }}</th>
+                        <td>{{ substr($post->title, 0, 50) }}{{ strlen($post->title) > 50 ? "..." : "" }}</td>
+                        <td>{{ substr($post->body, 0, 50) }}{{ strlen($post->body) > 50 ? "..." : "" }}</td>
+                        <td>{{date('M J, Y',strtotime( $post->created_at)) }}</td>
+                        <td><a href="{{ route('posts.show', $post->id)}}" class="btn btn-default btn-sm">View</a><a href="{{ route('posts.edit', $post->id)}}" class="btn btn-default btn-sm">Edit</a></td>
+
+                    </tr>
+
+                @endforeach
+            </tbody>
+
+
+        </table>
+
+
+    </div>
+
+
+@stop
